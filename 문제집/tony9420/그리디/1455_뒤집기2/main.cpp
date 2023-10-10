f8f99c32c8882e50fdb76d594f75e15cdb460a9e
Tin9oo <rlawnghks99@gmail.com>
2023-10-10 11:56:29 +0900

    clear_1455

--- 문제집/tony9420/그리디/1455_뒤집기2/main.cpp
+++ 문제집/tony9420/그리디/1455_뒤집기2/main.cpp
@@ -8,14 +8,14 @@ vector<vector<int>> C;
 
 int cnt = 0;
 
-void print_grid() {
-    for(int i = 0; i < C.size(); ++i) {
-        for(int j = 0; j < C[0].size(); ++j) {
-            cout << C[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
+// void print_grid() {
+//     for(int i = 0; i < C.size(); ++i) {
+//         for(int j = 0; j < C[0].size(); ++j) {
+//             cout << C[i][j] << " ";
+//         }
+//         cout << endl;
+//     }
+// }
 
 void flip(int a, int b) {
     for(int i = 0; i <= a; ++i) {
@@ -29,16 +29,12 @@ void flip(int a, int b) {
 }
 
 void solve() {
-    int j;
-    for(int i = C.size()-1; i >= 0; --i) {
-        j = i;
-        if(C[i][j]) flip(i, j);
-        for(j = i -1; j >= 0; --j) {
+    // 매 반복에서 최선의 선택이기 때문에 순차적으로 위로 올라가면 된다.
+    // 내가 기존 구현한 방식: 지점에서 왼쪽으로 쭉, 위로 쭉 올려서 몰아가는 방식 -> 메모리 소모 큼
+    for(int i = N - 1; i >= 0; --i) {
+        for(int j = M - 1; j >= 0; --j) {
             if(C[i][j]) {
                 flip(i, j);
-            }
-            if(C[j][i]) {
-                flip(j, i);
             }
         }
     }
